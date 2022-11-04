0bad72b1ae47f0b7a08cf710e4ac56539764eb9c
martinscorsese01 <mohsin.khan@ada.ac.uk>
2022-11-04 12:56:06 +0000

    make a guess

--- main.py
+++ main.py
@@ -2,7 +2,15 @@ import random
 
 num=0
 
-
 def generate_random_number():
   return random.randint(0-10)
 
+def difference_from_answer(guess, answer):
+  if guess < answer:
+    return "Too Low"
+  if guess > answer:
+    return "Too High"
+  
+
+def make_a_guess(answer):
+    
